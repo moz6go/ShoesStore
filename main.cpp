2b729cd5894aff19bd5e1cc0b5f14042f7fae70d
mzyblikevych <moz6go@gmail.com>
2019-04-14 13:48:01 +0300

    create sold foods table

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QApplication::setStyle(QStyleFactory::create("GTK+"));
+    //qDebug() << QStyleFactory::keys ();
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
     StoreMainWindow w;
     w.show();
     return a.exec();
